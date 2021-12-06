cf4518c22ed84a7741b50ce96e1739e7818f0ef1
kristiyan2021 <449302@student.saxion.nl>
2021-12-06 14:44:14 +0100

    Cleaned up the ClientThread class

--- src/main/java/nl/saxion/itech/server/ClientThread.java
+++ src/main/java/nl/saxion/itech/server/ClientThread.java
@@ -1,7 +1,5 @@
 package nl.saxion.itech.server;
 
-import nl.saxion.itech.server.model.Client;
-import nl.saxion.itech.server.model.InvalidUsernameException;
 import nl.saxion.itech.server.model.protocol.*;
 
 import java.io.BufferedReader;
@@ -9,49 +7,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class ClientThread extends Thread {
     private final Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
-    private Client client;
-    private ClientHandler clientHandler;
     private MessageVisitor messageVisitor;
     private MessageFactory messageFactory;
-
-    private boolean isConnected;
-    private String username;
-    private boolean receivedPong;
-
-    protected static final String CMD_CONN = "CONN";
-    protected static final String CMD_BCST = "BCST";
-    protected static final String CMD_OK = "OK";
-    protected static final String CMD_INFO = "INFO";
-    protected static final String CMD_PING = "PING";
-    protected static final String CMD_PONG = "PONG";
-    protected static final String CMD_QUIT = "QUIT";
-    protected static final String CMD_ER00 = "ER00";
-    protected static final String CMD_ER01 = "ER01";
-    protected static final String CMD_ER02 = "ER02";
-    protected static final String CMD_ER03 = "ER03";
 
     public ClientThread(Socket socket) {
         this.clientSocket = socket;
         try {
             this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
             this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-            this.clientHandler = ClientHandler.getInstance();
             this.messageVisitor = new MessageHandlerVisitor(this.clientSocket, this.out);
             this.messageFactory = new MessageFactory();
         }  catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
-
-        this.isConnected = false;
-        this.receivedPong = false;
     }
 
     public void run() {
@@ -73,10 +47,10 @@ public class ClientThread extends Thread {
         }
     }
 
-    private void sendPing() {
-        this.out.println(CMD_PING);
-        this.out.flush();
-    }
+//    private void sendPing() {
+//        this.out.println("PING");
+//        this.out.flush();
+//    }
 
     private String[] parseCommand(String input) {
         return input.split(" ", 2);
@@ -92,21 +66,21 @@ public class ClientThread extends Thread {
         }
     }
 
-    private void heartbeat() {
-        System.out.printf("~~ %s Heartbeat initiated\n", username);
-
-        CompletableFuture.delayedExecutor(10, TimeUnit.SECONDS).execute(() -> {
-            receivedPong = false;
-            sendPing();
-
-            CompletableFuture.delayedExecutor(3, TimeUnit.SECONDS).execute(() -> {
-                if (receivedPong) {
-                    System.out.printf("~~ %s Heartbeat expired - SUCCESS\n", username);
-                    heartbeat();
-                } else {
-                    System.out.printf("~~ %s Heartbeat expired - FAILED\n", username);
-                }
-            });
-        });
-    }
+//    private void heartbeat() {
+//        System.out.printf("~~ %s Heartbeat initiated\n", username);
+//
+//        CompletableFuture.delayedExecutor(10, TimeUnit.SECONDS).execute(() -> {
+//            receivedPong = false;
+//            sendPing();
+//
+//            CompletableFuture.delayedExecutor(3, TimeUnit.SECONDS).execute(() -> {
+//                if (receivedPong) {
+//                    System.out.printf("~~ %s Heartbeat expired - SUCCESS\n", username);
+//                    heartbeat();
+//                } else {
+//                    System.out.printf("~~ %s Heartbeat expired - FAILED\n", username);
+//                }
+//            });
+//        });
+//    }
 }
